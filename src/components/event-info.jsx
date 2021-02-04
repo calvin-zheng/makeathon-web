ae997bea5a976dddd275991b81aaa3b18d34c164
calzheng <calzheng@umich.edu>
2021-02-04 12:11:21 -0500

    Added longer desc

--- src/components/event-info.jsx
+++ src/components/event-info.jsx
@@ -24,11 +24,17 @@ class EventInfo extends Component {
                         <div className="flex-row" style={{width: '50%'}}>
                             <div className="flex-col" style={{width: '45%'}}>
                                 <h4>CAD (Computer-Aided Design)</h4>
-                                <p>The CAD Competition gives teams the opportunity to design a product using the CAD software and present it to the judges. Teams will have access to CAD tutorials and mentors throughout the competition. Feel free to be as creative as you’d like while building your product or simulation.</p>
+                                <p>The CAD Competition gives teams the opportunity to design a product using the CAD software and present it to the judges. Teams will have access to CAD tutorials and mentors throughout the competition. Feel free to be as creative as you’d like while building your product or simulation. Strive for a complex yet elegant design, with a focus on manufacturability.
+                                </p>
+                                 <p>   The goal of this competition is to create an innovative product that solves some problem or serves a purpose. You will create this product in CAD and try to make it as realistic and well constructed as possible. There are no limitations to this competition except that whatever product you make should be made in CAD.
+                                </p>
                             </div>
                             <div className="flex-col" style={{width: '45%'}}>
                                 <h4>Open Product Design</h4>
                                 <p>The OpenProduct Design Competition gives teams the freedom to design and build a physical product without any restrictions. This year, participants can purchase materials on their own that we will reimburse up to $125.</p>
+
+                                <p>The goal of this competition is to create an innovative product that solves some problem or serves a purpose. This product must be physically constructed, but the materials you use are up to you. Materials can be anything from Arduinos and Raspberry Pis to wood planks and metal sheets. The only limitation is the $125 we will reimburse.
+                                </p>
                             </div>
                         </div>
                     </div>
@@ -67,7 +73,8 @@ class EventInfo extends Component {
                         <br />
                         <div className="flex-col" style={{width: '25%'}}>
                             <h4>Autodesk Fusion 360 Award</h4>
-                            <p style={{margin: '5px'}}>This prize will be awarded to the two teams (from either competition) that best utilize the Autodesk Fusion 360 Software. You can find the judging rubric  <a href="https://docs.google.com/document/d/1yCTgJZe65scYPJXNgOvdxfqZDjuE1Gqg9S3X_iwQmFI/edit?usp=sharing"><i>here</i></a>.</p>
+                            <p style={{margin: '5px'}}>This is an award given to the two teams that utilize Fusion 360 software the best. You can enter into this award by using Fusion 360 in either competition. If you use Fusion 360, we recommend considering the criteria of your competitions’ rubric along with the criteria of the Fusion 360 award.
+                                You can find the judging rubric  <a href="https://docs.google.com/document/d/1yCTgJZe65scYPJXNgOvdxfqZDjuE1Gqg9S3X_iwQmFI/edit?usp=sharing"><i>here</i></a>.</p>
                             <div className="bullets">
                                 <div className="makebullet">
                                     <img src={makeicon} className="makebulletimg" alt="makeathon logo bullet"/>
